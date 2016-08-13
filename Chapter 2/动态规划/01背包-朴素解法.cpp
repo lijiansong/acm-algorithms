069ee046ddc9595e2cbc88c09db5275328937100
lijiansong <bat_ljs426@qq.com>
2016-08-13 11:09:29 +0800

    json lee

--- Chapter 2/动态规划/01背包-朴素解法.cpp
+++ Chapter 2/动态规划/01背包-朴素解法.cpp
@@ -3,7 +3,11 @@
 using namespace std;
 const int MAX_N=101;
 int weight[MAX_N]={0},value[MAX_N]={0},n,max_w;
-//
+//choose the part that total weight less than w from the i-th package
+int choose(int i,int w)
+{
+	
+}
 void solve()
 {
 	
