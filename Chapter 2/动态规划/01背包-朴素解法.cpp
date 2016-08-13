cdc7d48458e90d36c71d8a81ac5572865df4527b
lijiansong <bat_ljs426@qq.com>
2016-08-13 10:22:44 +0800

    json lee

---
+++ Chapter 2/动态规划/01背包-朴素解法.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+const int MAX_N=101;
+int weight[MAX_N]={0},value[MAX_N]={0},n,max_w;
+//
+void solve()
+{
+	
+}
+int main()
+{
+	while (cin>>n&&n)
+	{
+		for(int i=0;i<n;++i)
+			cin>>weight[i]>>value[i];
+		cin>>max_w;
+		solve();
+	}
+	return 0;
+}
