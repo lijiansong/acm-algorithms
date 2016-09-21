4a230a31e590f92eca227a2c52c815e2aedb936a
Json Lee <jiansongli426@gmail.com>
2016-09-21 22:12:27 +0800

    partition

--- GCJ/pivot.cpp
+++ GCJ/pivot.cpp
@@ -1,5 +1,13 @@
 #include<iostream>
 #include<algorithm>
+/*
+快排分区函数： 
+随机选择一个数作为枢纽元（pivot），这里选择第一个数，
+把比枢纽元小的数放在数组的左边，
+把大于等于枢纽元的数移到数组的右边，
+一趟遍历过后枢纽元归位， 
+返回枢纽元的位置下标 
+*/
 using namespace std;
 int partition(int a[],int start,int end)
 {
@@ -12,6 +20,26 @@ int partition(int a[],int start,int end)
 		if(a[i]<pivot)
 		{
 			swap(a[i],a[storeIndex]);
+			++storeIndex;
+		}
+	}
+	swap(a[storeIndex],a[end]);
+	return storeIndex;
+}
+int partition_(int a[],int start,int end)
+{
+	int pivotIndex=start;
+	int pivot=a[pivotIndex];
+	swap(a[pivotIndex],a[end]);
+	int storeIndex=start;
+	for(int i=0;i<end;++i)
+	{
+		if(a[i]<pivot)
+		{
+			if(i!=storeIndex)
+			{
+				swap(a[i],a[storeIndex]);
+			}
 			++storeIndex;
 		}
 	}
