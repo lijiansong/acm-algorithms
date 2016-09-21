004d9bd82cd256800ac21ff4da3525e0f9d36031
Json Lee <jiansongli426@gmail.com>
2016-09-21 16:28:16 +0800

    尺缩法 O(logn)

---
+++ GCJ/Subsequence-2.cpp
@@ -0,0 +1,48 @@
+/*
+	Name: 
+	Copyright: 
+	Author: 
+	Date: 21/09/16 16:27
+	Description: ³ßËõ·¨ O(n) 
+*/
+
+#include<iostream>
+#include<cstdio>
+#include<algorithm>
+using namespace std;
+const int MAX_N=100001;
+int n,s;
+int a[MAX_N];
+void solve()
+{
+	int res=n+1;
+	int ss=0,t=0,sum=0;
+	for(;;)
+	{
+		while(t<n && sum<s)
+		{
+			sum+=a[t++];
+		}
+		if(sum<s) break;
+		res=min(res,t-ss);
+		sum-=a[ss++];
+	}
+	if(res>n)
+	{
+		res=0;
+	}
+	printf("%d\n",res);
+}
+int main()
+{
+	freopen("sub.txt","r",stdin); 
+	while(scanf("%d %d",&n,&s)!=EOF)
+	{
+		for(int i=0;i<n;++i)
+		{
+			scanf("%d",&a[i]);
+		}
+		solve();
+	}
+	return 0;
+} 
