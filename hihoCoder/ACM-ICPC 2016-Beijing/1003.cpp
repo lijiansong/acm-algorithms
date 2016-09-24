4afe5d0292761890da801d7cedccdcaf61702a07
Json Lee <jiansongli426@gmail.com>
2016-09-24 18:17:15 +0800

    json lee

---
+++ hihoCoder/ACM-ICPC 2016-Beijing/1003.cpp
@@ -0,0 +1,116 @@
+#include<iostream>
+#include<cstdio>
+#include<string>
+#include<vector>
+#include<algorithm>
+using namespace std;
+typedef pair<string,int> p;
+string str="";
+vector<p> phrase;
+
+string init(const string &s)
+{
+	string ans=s;
+	for(int i=0;i< ans.length();++i)
+	{
+		if((ans[i]==' '&& ans[i+1]==' ')||(ans[i]==' '&& ans[i+1]==',')||(ans[i]==' '&& ans[i+1]=='.'))
+		{
+			ans.erase(i,1);
+		}
+	}
+	return ans;
+}
+bool cmp(const p &a,const p &b)
+{
+	return a.second > b.second;
+}
+
+void solve()
+{
+	string space=" ";
+	string t="";
+	int pos = str.find(space, 0);
+	cout<<pos<<endl;
+	while(pos!=-1)
+	{
+		if((str[pos+1]!=',')&&(str[pos+1]!='.'))
+		{
+			for(int i=pos-1;i>=0;--i)
+			{
+				if((str[i]==',')||(str[i]=='.')||(str[i]==' '))
+				{
+					t+=str.substr(i+1,pos-i-1);
+					//cout<<"--first t--"<<t<<endl;
+					break;
+				}
+				/*t+=str.substr(i,pos-i-1);
+				break;*/
+			}
+			
+			int offset=pos+1;
+			for(int i=offset;i<str.length();++i)
+			{
+				if(str[i]==' '||str[i]==','||str[i]=='.')
+				{
+					t+=str.substr(pos,i-pos);
+					//cout<<"----second t---"<<t<<endl;
+					p ph=make_pair(t,0);
+					phrase.push_back(ph);
+				}
+				/*else
+				{
+					t="";
+				}*/
+			}
+			pos = str.find(space, offset);
+		}
+		else
+		{
+			pos = str.find(space, pos+1);
+		}
+	}
+	
+	for(int i=0;i<phrase.size();++i)
+	{
+		int index=str.find(phrase[i].first,0);
+		while(index!=-1)
+		{
+			++phrase[i].second;
+			int offset=index+1;
+			index=str.find(phrase[i].first,offset);
+		}
+	}
+	sort(phrase.begin(),phrase.end(),cmp);
+	int times=phrase[0].second;
+	vector<string> s;
+	for(int i=0;i<phrase.size();++i)
+	{
+		if(phrase[i].second==times)
+			s.push_back(phrase[i].first);
+		else
+			break;
+	}
+	sort(s.begin(),s.end());
+	cout<<s[0]<<":"<<times<<endl;
+}
+int main()
+{
+	freopen("1003.txt","r",stdin);
+	string temp;
+	
+	while(getline(cin,temp))
+	{
+		if(temp!="####")
+		{
+			str +=temp;
+			str=init(str);
+			str+=".";
+		}
+		else
+		{
+			solve();
+			str="";
+		}
+	}
+	return 0;
+} 
---
+++ hihoCoder/ACM-ICPC 2016-Beijing/1009.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+vector<int>  ta,tb;
+vector<int> tac,tbc;
+int x,n,m;
+vector<int> da,db;
+int main()
+{
+	
+	return 0;
+} 
