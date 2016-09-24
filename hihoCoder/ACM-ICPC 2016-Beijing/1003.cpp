318efd5a8087c3132204cf72ad2a22ccb56e907c
Json Lee <jiansongli426@gmail.com>
2016-09-24 19:18:58 +0800

    fix bug

--- hihoCoder/ACM-ICPC 2016-Beijing/1003.cpp
+++ hihoCoder/ACM-ICPC 2016-Beijing/1003.cpp
@@ -30,7 +30,7 @@ void solve()
 	string space=" ";
 	string t="";
 	int pos = str.find(space, 0);
-	cout<<pos<<endl;
+	//cout<<pos<<endl;
 	while(pos!=-1)
 	{
 		if((str[pos+1]!=',')&&(str[pos+1]!='.'))
@@ -56,6 +56,10 @@ void solve()
 					//cout<<"----second t---"<<t<<endl;
 					p ph=make_pair(t,0);
 					phrase.push_back(ph);
+					cout<<t<<endl;
+					t="";
+					
+					break; 
 				}
 				/*else
 				{
@@ -69,6 +73,7 @@ void solve()
 			pos = str.find(space, pos+1);
 		}
 	}
+	pos=0;
 	
 	for(int i=0;i<phrase.size();++i)
 	{
@@ -90,8 +95,11 @@ void solve()
 		else
 			break;
 	}
+	
 	sort(s.begin(),s.end());
 	cout<<s[0]<<":"<<times<<endl;
+	phrase.clear();
+	s.clear();
 }
 int main()
 {
@@ -105,6 +113,7 @@ int main()
 			str +=temp;
 			str=init(str);
 			str+=".";
+			temp="";
 		}
 		else
 		{
