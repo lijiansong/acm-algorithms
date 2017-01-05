7ef75a83a53f6f6865343897c84a35d0765f21aa
Json Lee <jiansongli426@gmail.com>
2017-01-05 12:12:01 +0800

    next_permutation
    test

---
+++ CodeForces/next_permutation.cpp
@@ -0,0 +1,19 @@
+#include<cstdio>
+#include<algorithm>
+using namespace std;
+int main()
+{
+	int n,p[100];
+	scanf("%d",&n);
+	for(int i=0;i<n;++i)
+	{
+		scanf("%d",&p[i]);
+	}
+	sort(p,p+n);
+	do
+	{
+		for(int i=0;i<n;++i) printf("%d ",p[i]);
+		printf("\n");
+	}while(next_permutation(p,p+n));
+	return 0;
+} 
