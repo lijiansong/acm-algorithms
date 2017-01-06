5ab039dfdf52833ba219a8bf493b06af942e674c
Json Lee <jiansongli426@gmail.com>
2017-01-07 01:30:07 +0800

    bit vector

---
+++ CodeForces/bit_vector.cpp
@@ -0,0 +1,37 @@
+#include<cstdio>
+void print_subset(int n,int* a,int cur)
+{
+	/*for(int i=0;i<cur;++i) printf("%d ",a[i]);
+	printf("\n");
+	int s=cur?a[cur-1]+1:0;
+	for(int i=s;i<n;++i)
+	{
+		a[cur]=i;
+		print_subset(n,a,cur+1);
+	}*/
+	if(cur==n)
+	{
+		for(int i=0;i<cur;++i)
+		{
+			if(a[i]) printf("%d ",i);
+		}
+		printf("\n");
+		return;
+	}
+	a[cur]=1;
+	print_subset(n,a,cur+1);
+	a[cur]=0;
+	print_subset(n,a,cur+1);
+}
+int main()
+{
+	int n;
+	while(scanf("%d",&n)!=EOF)
+	{
+		int *a=new int[n];
+		//for(int i=1;i<=n;++i) a[i-1]=i;
+		print_subset(n,a,0);
+		delete a;
+	}
+	return 0;
+} 
